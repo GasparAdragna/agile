883c91c78184eda121777b93eafd0b29ba00dc22
Gaspar <gaspar.jac@hotmail.com>
2021-02-06 21:35:54 -0300

    Autorization and image display working

--- agile-vue/src/views/Home.vue
+++ agile-vue/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <h1>Test</h1>
+    <v-row>
+      <v-col v-for="photo in photos" :key="photo.id" cols="12" md="4">
+        <v-img :src="photo.cropped_picture"></v-img>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -9,21 +13,43 @@ const axios = require("axios");
 
 export default {
   name: "Home",
-  created: async function() {
-    let config = {
-      name: "Sherlock",
-      enemy: "Moriarty"
+  data() {
+    return {
+      token: null,
+      photos: [],
+      page: 0
     };
-    axios
-      .post("http://interview.agileengine.com/auth", {
-        params: config
-      })
-      .then(response => {
-        this.user = response.data;
-      })
-      .catch(e => {
-        console.log(e);
-      });
+  },
+  created: async function() {
+    if (this.token == null) {
+      try {
+        const response = await axios.post(
+          "http://interview.agileengine.com/auth",
+          {
+            apiKey: "23567b218376f79d9415"
+          }
+        );
+        this.token = response.data.token;
+        try {
+          const jsonPhotos = await axios.get(
+            "http://interview.agileengine.com/images",
+            {
+              headers: {
+                Authorization: "Bearer " + this.token
+              }
+            }
+          );
+          for (var i = 0; i < jsonPhotos.data.pictures.length; i++) {
+            this.photos.push(jsonPhotos.data.pictures[i]);
+          }
+          this.page = jsonPhotos.data.page;
+        } catch (e) {
+          console.log(e);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 };
 </script>
