e5ba5cdd1a3bb2a2c382a7e8fe71dafb3546ec25
Gaspar <gaspar.jac@hotmail.com>
2021-02-06 21:08:48 -0300

    Added Axios to project

--- agile-vue/src/App.vue
+++ agile-vue/src/App.vue
@@ -21,7 +21,7 @@
       <v-divider></v-divider>
 
       <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
--- agile-vue/src/views/Home.vue
+++ agile-vue/src/views/Home.vue
@@ -5,7 +5,25 @@
 </template>
 
 <script>
+const axios = require("axios");
+
 export default {
-  name: "Home"
+  name: "Home",
+  created: async function() {
+    let config = {
+      name: "Sherlock",
+      enemy: "Moriarty"
+    };
+    axios
+      .post("http://interview.agileengine.com/auth", {
+        params: config
+      })
+      .then(response => {
+        this.user = response.data;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
 };
 </script>
